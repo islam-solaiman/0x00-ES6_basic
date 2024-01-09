dcb9ac544b8a26689150bb7264d8d937f87b11f4
Islam Solaiman <islam.solaiman92@gmail.com>
2024-01-09 14:52:21 +0000

    Task 2. Filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation (arr, city) {
   if (arr instanceof Array) {
-    return arr.filter((el) => el.location != city);
+    return arr.filter((el) => el.location === city);
   }
   return [];
 }
