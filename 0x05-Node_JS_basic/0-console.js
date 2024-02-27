f03a0c7a22bb8a9281cf1288a5d4d6b38d9ae8c0
Islam Solaiman <islam.solaiman92@gmail.com>
2024-02-27 19:27:41 +0000

    0x05. NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * function named displayMessage that prints in STDOUT the string argument.
+ */
+
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
